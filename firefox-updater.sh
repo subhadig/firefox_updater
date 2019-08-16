f7e68c5583d34a8ee6195be3f43cec20ac15dc11
Subhadip Ghosh <subhadipdipghosh11@gmail.com>
2019-08-16 22:55:52 +0530

    Initial commit

---
+++ firefox-updater.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
+sudo tar xjvf FirefoxSetup.tar.bz2 -C /opt/ --overwrite
+rm FirefoxSetup.tar.bz2
